5696273d7892660487a198a5b7c2d00d2a942ea2
Darragh Downey <darragh.downey90@gmail.com>
2021-04-05 18:51:47 +1000

    Working on diophantine equation

---
+++ go/diophantine_eq/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+}
+
+func diophantineEq() {
+	// Find the value of D <= 1000 in minimal solutions of x for which
+	// the largest value of x is obtained
+	// x^2 - Dy^2 = 1
+}
